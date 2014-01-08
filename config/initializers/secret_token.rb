34061189ebddd70d5990c3df38159782a7a66438
Hajime Nagawa <h.nagawa@n-g-k.co.jp>
2014-01-08 21:39:47 +0900

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsWerckerHerokuHeiz123::Application.config.secret_key_base = '32011c884454da7dad42e590f1935525bb5321213f8e826757afe71bec7317acebbedee9193d01708905bc24a750d62b4c45156e63e227a0e3bf8e51082d5e1d'
